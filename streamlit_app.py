5b49179c2769a2b95e7c9476b0e461f48dd0ab2a
Tulasi <visala.kt@gmail.com>
2025-08-17 16:33:31 -0500

    indentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,27 +29,27 @@ ingredients_list = st.multiselect(
      max_selections = 5
 )
 if ingredients_list:  
-  
-   
-   ingredients_string = ''
+    ingredients_string = ''
 
-   for fruit_chosen in ingredients_list:
-       ingredients_string += fruit_chosen + ' '
-       search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-       #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-       st.subheader(fruit_chosen + ' Nutrtion Information')
-       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-       fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
+        st.subheader(fruit_chosen + ' Nutrtion Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
    #st.write(ingredients_string) 
 
-   my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order) values ('""" +ingredients_string+ """','"""+name_on_order+"""')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order) values ('""" +ingredients_string+ """','"""+name_on_order+"""')"""
    #st.write(my_insert_stmt)
    #st.stop()
 
-   time_to_submit = st.button('submit order',type="primary")
+    time_to_submit = st.button('submit order',type="primary")
                                
-   if time_to_submit:
+    if time_to_submit:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered, ' +name_on_order + '!', icon="✅")
 
