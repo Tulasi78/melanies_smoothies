02db95d9cf53f2048a98974987dbf568537750b2
Tulasi <visala.kt@gmail.com>
2025-08-17 11:00:18 -0500

    Update streamlit_app.py
    spelling mistake in froot instead fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ if ingredients_list:
    if ingredients_string:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
-    st.text(Smoothiefruit_response.json())  
+    st.text(Smoothiefroot_response.json())  
 
 
 
