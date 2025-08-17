35613aa9a92b396b909fc31162711cb9fa2c1b01
Tulasi <visala.kt@gmail.com>
2025-08-17 13:06:12 -0500

    added SEARCH_ON variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,12 +36,12 @@ if ingredients_list:
    for fruit_chosen in ingredients_list:
        ingredients_string += fruit_chosen + ' '
        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-       st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+       #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
        st.subheader(fruit_chosen + ' Nutrtion Information')
-       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-       #sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
+       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + SEARCH_ON)
+       sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
 
-   st.write(ingredients_string) 
+   #st.write(ingredients_string) 
 
    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" +ingredients_string+ """','"""+name_on_order+"""')"""
